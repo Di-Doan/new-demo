b717e6d6ce15120d342a892bf271cf49e97df094
di <dianthiendoan322@gmail.com>
2024-11-06 17:18:49 +0700

    update carousel

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -6,28 +6,17 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
   standalone: true,
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [NgbCarouselModule]
+  imports: [NgbCarouselModule],
 })
 export class HeaderComponent implements OnInit {
-  slides: any[] = new Array(4).fill({ id: -1, src: '', title: '', subtitle: '' });
+  slides: any[] = [
+    '../../../assets/img/banner1.png',
+    '../../../assets/img/banner2.png',
+    '../../../assets/img/banner3.png',
+    '../../../assets/img/banner4.png',
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-    this.slides[0] = {
-      src: '../../../assets/img/banner1.png'
-    };
-    this.slides[1] = {
-      src: '../../../assets/img/banner2.png'
-    };
-    this.slides[2] = {
-      src: '../../../assets/img/banner3.png'
-    };
-    this.slides[3] = {
-      src: '../../../assets/img/banner4.png'
-    };
-  }
-
-
-
+  ngOnInit() {}
 }
