623e1af404d16e6f7c07acf5c99d215f465af0ba
di <dianthiendoan322@gmail.com>
2024-11-06 17:10:16 +0700

    update carousel

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselConfig, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule, NgbModule, ReactiveFormsModule)]
+  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule, NgbModule, ReactiveFormsModule, NgbCarouselConfig)]
 };
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss'],
+  imports: [NgbCarouselModule]
 })
 export class HeaderComponent implements OnInit {
+  slides: any[] = new Array(4).fill({ id: -1, src: '', title: '', subtitle: '' });
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.slides[0] = {
+      src: '../../../assets/img/banner1.png'
+    };
+    this.slides[1] = {
+      src: '../../../assets/img/banner2.png'
+    };
+    this.slides[2] = {
+      src: '../../../assets/img/banner3.png'
+    };
+    this.slides[3] = {
+      src: '../../../assets/img/banner4.png'
+    };
   }
+
+
 
 }
